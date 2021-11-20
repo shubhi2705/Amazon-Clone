650c22508c0567ab430243ec8bbea3f9fb071b40
Shubhi2705 <shubhisinghal2705@gmail.com>
2021-11-20 16:41:22 +0530

    Added Product screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,25 +7,25 @@ const Home = () => {
         <div className='home'>
             <div className='home__container'>
                 <img className='home__image' src='https://m.media-amazon.com/images/I/61FuWeCuGCL._SX3000_.jpg'/>
-                <div className="home__rows">
-
+                <div className='home__rows'>
                 <div className='home__row'>
-                    <Product />
-                    <Product />
-                </div>
-                <div className='home__row'>
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product rating={4} title="The Lean Startup-How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses" price={1,150} image='https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY327_FMwebp_QL65_.jpg' />
+                    <Product   rating={5} title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal with Sengled Bluetooth Color bulb" price={1918} image="https://m.media-amazon.com/images/I/7144xNeL4iS._AC_UY327_FMwebp_QL65_.jpg"/>
                 </div>
                 <div className='home__row'>
-                    <Product />    
+                    <Product  rating={5}/>
+                    <Product  rating={3}/>
+                    <Product  rating={3}/>
                 </div>
+                <div className='home__row'>
+                    <Product  rating={4}/>    
+                </div> 
                 </div>
-              
+               
             </div>
         </div>
     )
 }
 
 export default Home
+//
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import './Product.css'
-const Product = () => {
+const Product = ({rating,title,image,price}) => {
     return (
         <div className='product__container'>
             <div className='product__info'>
-                <p className='product__name'>The lean startup</p>
+                <p className='product__name'>{title}</p>
                 <p className='product__price'>
                     <small>Rs.</small>
-                    <strong>199.00</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className='product__rating'>
+                {
+                Array(rating)
+                .fill()
+                .map((_,i)=>(
                 <span class="fa fa-star checked"> </span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>         
+                ))}
+                       
                </div>
              
             </div>
-            <img  src='https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY327_FMwebp_QL65_.jpg'></img>
+            <img  src={image}></img>
               <button className='product__btn'>Add to Basket</button>
         </div>
     )
